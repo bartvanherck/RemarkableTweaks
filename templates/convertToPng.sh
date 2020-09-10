962ce6f2acc12c85ad3ac9e89dd74d17a3a54faf
Bart Vanherck <vherckbart@gmail.com>
2020-09-10 22:44:18 +0200

    Script to convert to png

---
+++ templates/convertToPng.sh
@@ -0,0 +1,6 @@
+convert Checklijst.svg Checklijst.png
+convert Review.svg Review.png
+convert Sections.svg Sections.png
+convert Sprint.svg Sprint.png
+convert Story.svg Story.png
+convert ChecklijstNoHeader.svg ChecklijstNoHeader.png
